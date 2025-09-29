9a01b37106ddbd02cc8b278a5a74e31bfb7d2c62
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-09-29 18:41:52 -0500

    Finished navbar, added redirections and user information about his level.

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,10 +1,9 @@
 
 
-import { authUser } from "~/auth/GetUser"
+import { authUser } from "~/user/UserHandler"
 import { Motion, Presence } from "solid-motionone"
-import { createSignal } from "solid-js"
+import { createSignal, onMount } from "solid-js"
 import { JSX, Show } from "solid-js"
-import h from "solid-js/h"
 
 
 const NButton = (props: { children?: JSX.Element, redirect: string }) => {
@@ -28,7 +27,7 @@ const NButton = (props: { children?: JSX.Element, redirect: string }) => {
             onclick={(event) => {
                 event.stopPropagation()
                 handleClick(event)
-            }} class=" bg-[var(--surface)]  text-2xl  text-[var(--color-tertiary)] cursor-pointer w-10 h-10 rounded-full" >{props.children}</Motion.button>
+            }} class=" bg-[var(--surface)]  text-2xl  text-[var(--color-secondary)] cursor-pointer w-10 h-10 rounded-full" >{props.children}</Motion.button>
     )
 }
 
@@ -48,8 +47,12 @@ const Navbar = () => {
         timeoutId = setTimeout(() => {
             setActive(false);
             timeoutId = null;
-        }, 4000) as unknown as number;
+        }, 5000) as unknown as number;
     };
+
+    onMount(() => {
+        handleClick()
+    });
 
     return (
         <Motion.nav
@@ -59,23 +62,29 @@ const Navbar = () => {
                 event.stopPropagation()
                 handleClick()
             }}
-            class="bg-[var(--color-primary)] py-2 flex justify-center items-center fixed bottom-0 left-1/2 -translate-x-1/2 p-2 w-[90%] md:w-[60%] rounded-t-3xl! z-[50] overflow-hidden select-none" >
+            class="cursor-pointer bg-[var(--color-primary)] border-[var(--surface)] border-t-2 border-x-2 py-2 flex justify-center items-center fixed bottom-0 left-1/2 -translate-x-1/2 p-2 w-[90%] md:w-[60%] rounded-t-3xl! z-[50] overflow-hidden select-none" >
 
             <Presence exitBeforeEnter>
                 <Show when={active()} keyed>
                     <Motion.div
                         initial={{ width: "30%" }}
-                        animate={{ width: active() ? "68%" : "30%" }}
+                        animate={{ width: active() ? "40vh" : "30%" }}
                         transition={{ duration: 0.3, easing: [0.34, 1.56, 0.64, 1] }}
                         class="w-full flex justify-between">
-                        <NButton redirect=""><i class="fa-solid fa-crown"></i></NButton>
-                        <NButton redirect=""><i class="fa-solid fa-gear"></i></NButton>
+                        <NButton redirect="/levels"><i class="fa-solid fa-crown"></i></NButton>
+                        <NButton redirect="/settings"><i class="fa-solid fa-gear"></i></NButton>
                         <div class="flex flex-col justify-center items-center relative">
-                            <img loading="lazy" class="w-10 h-10 rounded-full" src={authUser()?.user_metadata.avatar_url}></img>
-                            <p class="text-xs absolute translate-y-[100%] mt-5 text-[var(--surface)]">LVL 1</p>
+                            <img
+                                onpointerdown={(e) => {
+                                    e.stopPropagation();
+                                    window.location.href = `/users/${authUser()?.id}`
+                                }}
+                                loading="lazy"
+                                class="w-10 h-10 rounded-full cursor-pointer" src={authUser()?.user_metadata.avatar_url}></img>
+                            <p class="text-xs absolute translate-y-[100%] mt-5 text-[var(--surface)]">LVL {authUser()?.user_metadata.level}</p>
                         </div>
-                        <NButton redirect="#searchBar"><i class="fa-solid fa-magnifying-glass"></i></NButton>
-                        <NButton redirect="#myPost"><i class="fa-solid fa-plus"></i></NButton>
+                        <NButton redirect="/discover"><i class="fa-solid fa-magnifying-glass"></i></NButton>
+                        <NButton redirect="/discover"><i class="fa-solid fa-plus"></i></NButton>
                     </Motion.div>
                 </Show>
             </Presence>
