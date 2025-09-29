3c32ed7c4448d7c2d50ad65e481e6faa22e09784
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-09-29 18:40:13 -0500

    Added signature, user handler, notifications and a new modal to app.

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,27 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+import UserHandler from "~/user/UserHandler";
+import Notifications from "~/components/Notifications";
+import Signature from "~/components/Signature";
 import "~/styles/global.scss";
 import "~/styles/tailwind.css";
-import GetUser from "./auth/GetUser";
+import ModalSetter from "./components/modal";
 
 export default function App() {
   return (
-    <>
-      <GetUser />
-      <Router
-        root={props => (
-          <>
-            <Suspense>{props.children}</Suspense>
-          </>
-        )}
-      >
-        <FileRoutes />
-      </Router>
-    </>
+    <Router
+      root={props => (
+        <>
+          <Signature />
+          <UserHandler />
+          <Notifications />
+          <ModalSetter />
+          <Suspense>{props.children}</Suspense>
+        </>
+      )}
+    >
+      <FileRoutes />
+    </Router>
   );
 }
