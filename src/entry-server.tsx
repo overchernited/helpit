5bb69a8fe911ff875fa4e1c75ac7beaca499fe12
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-09-29 18:43:47 -0500

    Added another signature :)

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -15,6 +15,12 @@ export default createHandler(() => (
         <body>
           <div id="app">{children}</div>
           {scripts}
+          <div innerHTML="<!-- _                     _ _           _ 
+  _____   _____ _ __ ___| |__   ___ _ __ _ __ (_) |_ ___  __| |
+ / _ \ \ / / _ \ '__/ __| '_ \ / _ \ '__| '_ \| | __/ _ \/ _` |
+| (_) \ V /  __/ | | (__| | | |  __/ |  | | | | | ||  __/ (_| |
+ \___/ \_/ \___|_|  \___|_| |_|\___|_|  |_| |_|_|\__\___|\__,_| -->">
+          </div>
         </body>
         <script src="https://kit.fontawesome.com/a79f7121ff.js" crossorigin="anonymous"></script>
       </html>
