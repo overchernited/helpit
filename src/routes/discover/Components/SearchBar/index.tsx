6f58a8a83b1380c653be5a33f9c4a9443e43bff5
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-09-29 18:47:35 -0500

    Styles updated

--- src/routes/discover/Components/SearchBar/index.tsx
+++ src/routes/discover/Components/SearchBar/index.tsx
@@ -10,7 +10,7 @@ const SearchBar = () => {
                 scale: isFocused() ? 1.2 : 1,
             }}
             id="searchBar"
-            class="palette-gradient rounded-2xl h-[5vh] w-[50vw] my-5 p-5 flex flex-row items-center gap-2" >
+            class="palette-gradient rounded-2xl h-[5vh] w-[90vw] md:w-[50vw] my-2 p-5 flex flex-row items-center gap-2" >
             <i class="fa-solid fa-magnifying-glass text-palette-gradient"></i>
             <input type="text" onblur={() => { setFocus(false) }} onfocus={() => { setFocus(true) }} placeholder="Buscar..." class="outline-none w-full text-[var(--color-primary)] font-bold" />
         </Motion.div >
