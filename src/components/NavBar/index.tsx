acc5e566dac35e6fbc1b3d2666c8dc92006e0556
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-10-03 12:06:45 -0500

    navbar now always visible improving ux

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -24,6 +24,9 @@ const NButton = (props: { children?: JSX.Element, redirect: string }) => {
     return (
         <Motion.button
             hover={{ scale: 1.3 }}
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ easing: [0.34, 1.56, 0.64, 1] }}
             onclick={(event) => {
                 event.stopPropagation()
                 handleClick(event)
@@ -37,56 +40,34 @@ const Navbar = () => {
     const [active, setActive] = createSignal(false)
     let timeoutId: number | null = null;
 
-    const handleClick = () => {
-        setActive(!active());
-        if (active() === false) {
-            if (timeoutId) clearTimeout(timeoutId);
-            return;
-        }
-        if (timeoutId) clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            setActive(false);
-            timeoutId = null;
-        }, 5000) as unknown as number;
-    };
-
-    onMount(() => {
-        handleClick()
-    });
-
     return (
         <Motion.nav
-            initial={{ height: "4rem" }}
-            animate={{ height: active() ? "4rem" : "1.5rem" }}
-            onclick={(event) => {
-                event.stopPropagation()
-                handleClick()
-            }}
-            class="cursor-pointer bg-[var(--color-primary)] text-[var(--font-color)] border-[var(--font-color)] border-t-2 border-x-2 py-2 flex justify-center items-center fixed bottom-0 left-1/2 -translate-x-1/2 p-2 w-[90%] md:w-[60%] rounded-t-3xl! z-[50] overflow-hidden select-none" >
+            initial={{ height: "2rem" }}
+            animate={{ height: "4rem" }}
+            transition={{ delay: 0.1 }}
+            class=" bg-[var(--color-primary)] text-[var(--font-color)] border-[var(--font-color)] border-t-2 border-x-2 py-2 flex justify-center items-center fixed bottom-0 left-1/2 -translate-x-1/2 p-2 w-[90%] md:w-[60%] rounded-t-3xl! z-[50] overflow-hidden select-none" >
 
             <Presence exitBeforeEnter>
-                <Show when={active()} keyed>
-                    <Motion.div
-                        initial={{ width: "30%" }}
-                        animate={{ width: active() ? "40vh" : "30%" }}
-                        transition={{ duration: 0.3, easing: [0.34, 1.56, 0.64, 1] }}
-                        class="w-full flex justify-between">
-                        <NButton redirect="/levels"><i class="fa-solid fa-crown"></i></NButton>
-                        <NButton redirect="/settings"><i class="fa-solid fa-gear"></i></NButton>
-                        <div class="flex flex-col justify-center text-[var(--font-color)] items-center relative">
-                            <img
-                                onpointerdown={(e) => {
-                                    e.stopPropagation();
-                                    window.location.href = `/users/${authUser()?.id}`
-                                }}
-                                loading="lazy"
-                                class="w-auto object-cover h-10 rounded-full cursor-pointer" src={authUser()?.user_metadata.avatar_url}></img>
-                            <p class="text-xs absolute translate-y-[100%] mt-5">LVL {authUser()?.user_metadata.level}</p>
-                        </div>
-                        <NButton redirect="/discover"><i class="fa-solid fa-magnifying-glass"></i></NButton>
-                        <NButton redirect="/flubberai"><i class="fa-solid fa-robot"></i></NButton>
-                    </Motion.div>
-                </Show>
+                <Motion.div
+                    initial={{ width: "20vh" }}
+                    animate={{ width: "40vh" }}
+                    transition={{ duration: 0.3, easing: [0.34, 1.56, 0.64, 1] }}
+                    class="w-full flex justify-between">
+                    <NButton redirect="/levels"><i class="fa-solid fa-crown"></i></NButton>
+                    <NButton redirect="/settings"><i class="fa-solid fa-gear"></i></NButton>
+                    <div class="flex flex-col justify-center text-[var(--font-color)] items-center relative">
+                        <img
+                            onpointerdown={(e) => {
+                                e.stopPropagation();
+                                window.location.href = `/users/${authUser()?.id}`
+                            }}
+                            loading="lazy"
+                            class="w-auto object-cover h-10 rounded-full cursor-pointer" src={authUser()?.user_metadata.avatar_url}></img>
+                        <p class="text-xs absolute translate-y-[100%] mt-5">LVL {authUser()?.user_metadata.level}</p>
+                    </div>
+                    <NButton redirect="/discover"><i class="fa-solid fa-magnifying-glass"></i></NButton>
+                    <NButton redirect="/flubberai"><i class="fa-solid fa-robot"></i></NButton>
+                </Motion.div>
             </Presence>
         </Motion.nav>
     )
