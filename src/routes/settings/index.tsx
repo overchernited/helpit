b51e0f9ba6842225552d136e24e069bb430a8e2a
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-10-02 21:56:10 -0500

    Privatized page to avoid users not authenticated enter.

--- src/routes/settings/index.tsx
+++ src/routes/settings/index.tsx
@@ -2,6 +2,7 @@ import { createSignal, onMount, Show } from "solid-js";
 import { Motion } from "solid-motionone";
 import Button from "~/components/Button";
 import Navbar from "~/components/NavBar";
+import Private from "~/components/Routes";
 import supa from "~/lib/supabase";
 import { setTheme } from "~/store/theme";
 import { authUser } from "~/user/UserHandler";
@@ -57,55 +58,57 @@ const Settings = () => {
 
     return (
         <>
-            <Navbar />
-            <div class="h-screen w-screen bg-[var(--background-alt)] text-[var(--font-color)] items-center flex flex-col">
-                <main class="flex flex-col items-center my-5 gap-5">
-                    <h1 class="text-4xl font-bold underline">Ajustes</h1>
-                    <section class="flex flex-col items-center">
-                        <img width={150} height={150} class="rounded-lg" src={authUser()?.user_metadata.avatar_url}></img>
-                        <h1 class="font-bold text-xl">{authUser()?.user_metadata.full_name}</h1>
-                        <Button class="my-2" btnStyle="button-palette" onClick={handleSignOut}>Cerrar sesión</Button>
-                    </section>
+            <Private endpoint="/login">
+                <Navbar />
+                <div class="h-screen w-screen bg-[var(--background-alt)] text-[var(--font-color)] items-center flex flex-col">
+                    <main class="flex flex-col items-center my-5 gap-5">
+                        <h1 class="text-4xl font-bold underline">Ajustes</h1>
+                        <section class="flex flex-col items-center">
+                            <img width={150} height={150} class="rounded-lg" src={authUser()?.user_metadata.avatar_url}></img>
+                            <h1 class="font-bold text-xl">{authUser()?.user_metadata.full_name}</h1>
+                            <Button class="my-2" btnStyle="button-palette" onClick={handleSignOut}>Cerrar sesión</Button>
+                        </section>
 
-                    <form onsubmit={(e: Event) => handleSubmit(e)} class="text-center">
-                        <article>
-                            <p class="font-bold text-3xl underline">Apariencia</p>
-                            <section class="flex flex-row items-center gap-5 my-5">
-                                <div>
-                                    <input type="checkbox" checked={darkTheme()} class="hidden" />
-                                    <Motion.div
-                                        animate={darkTheme() ? { scale: 1.2 } : { scale: 1 }}
-                                        onClick={toggleDarkTheme}
-                                        class={`w-8 h-8 p-3 cursor-pointer text-[var(--font-color-alt-2)] flex items-center justify-center rounded border-2 border-[var(--color-tertiary)] }`}
-                                    >
-                                        <Show when={darkTheme()}>
-                                            <i class="fa-solid fa-check text-2xl"></i>
-                                        </Show>
-                                    </Motion.div>
-                                </div>
-                                <p class="font-regulr text-2xl">Activar modo oscuro</p>
-                            </section>
-                            <section class="flex flex-row items-center gap-5 my-5">
-                                <div>
-                                    <input type="checkbox" checked={highContrast()} class="hidden" />
-                                    <Motion.div
-                                        animate={highContrast() ? { scale: 1.2 } : { scale: 1 }}
-                                        onClick={toggleHighContrast}
-                                        class={`w-8 h-8 p-3 cursor-pointer flex items-center justify-center rounded border-2 text-[var(--font-color-alt-2)] border-[var(--color-tertiary)] }`}
-                                    >
-                                        <Show when={highContrast()}>
-                                            <i class="fa-solid fa-check text-2xl"></i>
-                                        </Show>
-                                    </Motion.div>
-                                </div>
-                                <p class="font-regulr text-2xl">Activar modo de alto contraste</p>
-                            </section>
+                        <form onsubmit={(e: Event) => handleSubmit(e)} class="text-center">
+                            <article>
+                                <p class="font-bold text-3xl underline">Apariencia</p>
+                                <section class="flex flex-row items-center gap-5 my-5">
+                                    <div>
+                                        <input type="checkbox" checked={darkTheme()} class="hidden" />
+                                        <Motion.div
+                                            animate={darkTheme() ? { scale: 1.2 } : { scale: 1 }}
+                                            onClick={toggleDarkTheme}
+                                            class={`w-8 h-8 p-3 cursor-pointer text-[var(--font-color-alt-2)] flex items-center justify-center rounded border-2 border-[var(--color-tertiary)] }`}
+                                        >
+                                            <Show when={darkTheme()}>
+                                                <i class="fa-solid fa-check text-2xl"></i>
+                                            </Show>
+                                        </Motion.div>
+                                    </div>
+                                    <p class="font-regulr text-2xl">Activar modo oscuro</p>
+                                </section>
+                                <section class="flex flex-row items-center gap-5 my-5">
+                                    <div>
+                                        <input type="checkbox" checked={highContrast()} class="hidden" />
+                                        <Motion.div
+                                            animate={highContrast() ? { scale: 1.2 } : { scale: 1 }}
+                                            onClick={toggleHighContrast}
+                                            class={`w-8 h-8 p-3 cursor-pointer flex items-center justify-center rounded border-2 text-[var(--font-color-alt-2)] border-[var(--color-tertiary)] }`}
+                                        >
+                                            <Show when={highContrast()}>
+                                                <i class="fa-solid fa-check text-2xl"></i>
+                                            </Show>
+                                        </Motion.div>
+                                    </div>
+                                    <p class="font-regulr text-2xl">Activar modo de alto contraste</p>
+                                </section>
 
-                            <Button btnStyle="button-palette">Aplicar Cambios</Button>
-                        </article>
-                    </form>
-                </main>
-            </div >
+                                <Button type="submit" btnStyle="button-palette">Aplicar Cambios</Button>
+                            </article>
+                        </form>
+                    </main>
+                </div >
+            </Private>
         </>
     )
 }
