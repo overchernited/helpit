e7ab6bf16473d7e5fa677628261e7d72d01356c3
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-10-06 14:27:22 -0500

    Open post when post_id is passed in params.

--- src/routes/discover/index.tsx
+++ src/routes/discover/index.tsx
@@ -7,18 +7,51 @@ import Navbar from "~/components/NavBar";
 import MyPost from "./Components/Posts/MyPost";
 import Categories from "./Components/Posts/Categories";
 import { category } from "./Components/Posts/Categories";
-import { createSignal, Show } from "solid-js";
+import { createSignal, onMount, Show } from "solid-js";
 import { userSignUpState } from "~/user/UserHandler";
 import { authUser } from "~/user/UserHandler";
+import NotificationsButton from "../Notifications/NotificationButton";
+import { useParams, useSearchParams } from "@solidjs/router";
+import supa from "~/lib/supabase";
+import ExpandedPost from "./Components/Posts/Post/expandedpost";
+import { openModal } from "~/store/modal";
 
 
 const Discover = () => {
 
+
     const [refetchSignal, setRefetchSignal] = createSignal(0);
+    const [params, setParams] = useSearchParams()
+
+
+
+    const getParamPost = async () => {
+        console.log(params.post_id);
+        if (!params.post_id) {
+            return;
+        }
+        try {
+            const { data, error } = await supa.from("posts").select("*").eq("id", params.post_id).single();
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            openModal({ content: () => <ExpandedPost {...data} /> })
+        }
+        catch (error) {
+            throw error
+        }
+    }
+
+    onMount(() => {
+        getParamPost();
+    })
 
     return (
         <>
             <Private endpoint="/">
+                <NotificationsButton />
                 <p class="text-[var(--font-color-alt)] text-xs m-2 font-extralight">HELPIT V1.0</p>
                 <Navbar />
                 <div class=" flex items-center flex-col my-4 gap-2 bg-[var(--background)]">
@@ -46,8 +79,8 @@ const Discover = () => {
                     <div class="flex justify-center h-auto w-[90vw] md:w-[50vw] " >
                         <MyPost onCreate={() => setRefetchSignal((prev) => setRefetchSignal(prev + 1))} />
                     </div >
+                    <h1 class="text-4xl text-center font-bold text-[var(--font-color-alt)] mt-2">Bienvenidx de vuelta!</h1>
                     <Categories />
-                    <h1 class="text-4xl text-center font-bold text-[var(--font-color-alt)]">Bienvenidx de vuelta!</h1>
                     <PostLoader category={category()} signal={refetchSignal()} />
                 </div>
             </Private >
